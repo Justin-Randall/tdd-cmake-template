8ec8b2858d28644f030e7911a0fa217c189c257c
Justin Randall <exabytecoder@gmail.com>
2024-07-07 08:28:26 +0800

    pacify coverage not picked up in test

--- tests/test_helpers_measure_execution_time.cpp
+++ tests/test_helpers_measure_execution_time.cpp
@@ -6,23 +6,14 @@
 
 class MeasureExecutionTimeTest : public ::testing::Test {
 public:
-  // Setup function that does nothing
-  void NoOpSetup(size_t) {}
-
-  // Lambda function that sleeps for a fixed amount of time (10ms)
-  void SleepLambda(size_t) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-  }
 };
 
 TEST_F(MeasureExecutionTimeTest, MeasuresCorrectAverageTime) {
   size_t input_size = 0; // Not used in NoOpSetup or SleepLambda
   size_t repetitions = 5;
   double measured_time =
-      measure_execution_time(std::bind(&MeasureExecutionTimeTest::NoOpSetup,
-                                       this, std::placeholders::_1),
-                             std::bind(&MeasureExecutionTimeTest::SleepLambda,
-                                       this, std::placeholders::_1),
+      measure_execution_time([](size_t){},
+                             [](size_t){std::this_thread::sleep_for(std::chrono::milliseconds(1));},
                              input_size, repetitions);
 
   // Allow some margin for error in timing
